28956dcee071271505c931fb160c43af001b3d81
luis <luishm2024@gmail.com>
2023-12-13 14:42:06 -0500

    creating project

---
+++ src/main/java/com/sunset/rider/msvclabhotel/MsvcLabHotelApplication.java
@@ -0,0 +1,13 @@
+package com.sunset.rider.msvclabhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcLabHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcLabHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunset/rider/msvclabhotel/MsvcLabHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunset.rider.msvclabhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcLabHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
